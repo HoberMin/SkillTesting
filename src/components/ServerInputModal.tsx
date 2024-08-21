e3331ec7209ab17beb05feb338f3522a502274ca
songhaeunsong <gkdms3469@naver.com>
2024-08-21 14:00:31 +0900

    feat: 모달 open & close 및 submit(console)

--- src/components/ServerInputModal.tsx
+++ src/components/ServerInputModal.tsx
@@ -1,8 +1,11 @@
 import { useState } from 'react';
 
+import { DialogDescription } from '@radix-ui/react-dialog';
+
 import {
   Dialog,
   DialogContent,
+  DialogHeader,
   DialogTitle,
   DialogTrigger,
 } from './shadcn/dialog';
@@ -11,6 +14,7 @@ import { Tabs, TabsContent, TabsList, TabsTrigger } from './shadcn/tabs';
 const ServerInputModal = () => {
   const [localURL, setLocalURL] = useState('');
   const [deployedURL, setDeployedURL] = useState('');
+  const [isOpenedModal, setIsOpenedModal] = useState(true);
 
   const handleInputChange = (
     e: React.ChangeEvent<HTMLInputElement>,
@@ -19,37 +23,79 @@ const ServerInputModal = () => {
     setState(e.target.value);
   };
 
+  const activeEnter = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === 'Enter') onsubmitURL();
+  };
+
+  const onsubmitURL = () => {
+    console.log(localURL ? localURL : deployedURL);
+    setIsOpenedModal(false);
+
+    setLocalURL('');
+    setDeployedURL('');
+  };
+
+  // TODO
+  // 서버 url에 대한 정보가 있으면 모달을 안 열고
+  // 정보가 없으면 모달 열기
+
+  // 탭이 바뀔 때는 입력값이 초기화 되는 게 좋지 않을까?
+
   return (
-    <Dialog defaultOpen={true}>
-      <DialogTrigger>
-        <button className='rounded-[5px] border-[1px] p-2'>
-          Edit Base URL
-        </button>
+    <Dialog open={isOpenedModal} onOpenChange={setIsOpenedModal}>
+      <DialogTrigger
+        onClick={() => setIsOpenedModal(!isOpenedModal)}
+        className='rounded-[5px] border p-2 text-[12px]'
+      >
+        Edit Base URL
       </DialogTrigger>
       <DialogContent>
-        <DialogTitle>Base URL</DialogTitle>
+        <DialogHeader>
+          <DialogTitle>Base URL</DialogTitle>
+          <DialogDescription className='text-[12px]'>
+            개발한 서버의 Base URL을 입력해주세요.
+          </DialogDescription>
+        </DialogHeader>
         <Tabs defaultValue='local'>
           <TabsList className='grid w-full grid-cols-2'>
             <TabsTrigger value='local'>Local</TabsTrigger>
             <TabsTrigger value='deployed'>Deployed</TabsTrigger>
           </TabsList>
           <TabsContent value='local'>
-            <input
-              className='grid w-full grid-cols-2 rounded-[5px] border-2 p-3'
-              type='text'
-              value={localURL}
-              onChange={e => handleInputChange(e, setLocalURL)}
-              placeholder='http://localhost:8080'
-            />
+            <div className='flex gap-2'>
+              <input
+                className='grid w-full grid-cols-2 rounded-[5px] border p-3'
+                type='text'
+                value={localURL}
+                onChange={e => handleInputChange(e, setLocalURL)}
+                onKeyDown={e => activeEnter(e)}
+                placeholder='http://localhost:8080'
+              />
+              <button
+                className='w-14 rounded-[5px] border text-[12px]'
+                onClick={onsubmitURL}
+              >
+                Go!
+              </button>
+            </div>
           </TabsContent>
           <TabsContent value='deployed'>
-            <input
-              className='grid w-full grid-cols-2 rounded-[5px] border-2 p-3'
-              type='text'
-              value={deployedURL}
-              onChange={e => handleInputChange(e, setDeployedURL)}
-              placeholder='https://abcde.com'
-            />
+            <div className='flex gap-2'>
+              <input
+                className='grid w-full grid-cols-2 rounded-[5px] border p-3'
+                type='text'
+                value={deployedURL}
+                onChange={e => handleInputChange(e, setDeployedURL)}
+                onKeyDown={e => activeEnter(e)}
+                placeholder='https://abcde.com'
+              />
+              <button
+                className='w-16 rounded-[5px] border text-[12px]'
+                onClick={onsubmitURL}
+              >
+                Go!
+              </button>
+            </div>
           </TabsContent>
         </Tabs>
       </DialogContent>
