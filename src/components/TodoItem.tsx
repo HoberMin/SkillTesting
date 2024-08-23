cda77078db61af2897a2893313d9bb065c97a438
HoberMin <102784200+HoberMin@users.noreply.github.com>
2024-08-23 15:25:29 +0900

    feat: completed값 제어

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -2,6 +2,7 @@ import { patchTodoApi } from '@/apis/todo';
 
 import TodoEditIcon from './TodoEditIcon';
 import { Switch } from './shadcn/switch';
+import { useToast } from './shadcn/toast/use-toast';
 
 interface TodoItemProps {
   checked: boolean;
@@ -10,8 +11,16 @@ interface TodoItemProps {
 }
 const TodoItem = ({ checked, todo, todoId }: TodoItemProps) => {
   const patchTodo = patchTodoApi();
-
+  const { toast } = useToast();
   const handleTodoPut = () => {
+    if (checked === undefined) {
+      toast({
+        variant: 'destructive',
+        title: 'Switch 에러',
+        description: 'completed값이 없습니다 !',
+      });
+      return;
+    }
     patchTodo(todoId);
   };
 
@@ -19,7 +28,7 @@ const TodoItem = ({ checked, todo, todoId }: TodoItemProps) => {
     <div className='px-8 py-4'>
       <div className='flex items-center justify-between font-medium'>
         <div className='flex items-center gap-2'>
-          <Switch checked={checked} onClick={handleTodoPut} />
+          <Switch checked={checked || false} onClick={handleTodoPut} />
           <span>{todo}</span>
         </div>
         <TodoEditIcon todoId={todoId} />
