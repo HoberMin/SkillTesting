8fa1d98b1a3d247450e28b5112fb3c4f00dc946f
HoberMin <102784200+HoberMin@users.noreply.github.com>
2024-09-02 16:55:59 +0900

    feat: InfinityScroll 구현

---
+++ src/apis/infinityScroll.ts
@@ -0,0 +1,54 @@
+import { useInfiniteQuery } from '@tanstack/react-query';
+
+import { getDomain } from '@/utils/domain';
+
+interface Todo {
+  title: string;
+  completed: boolean;
+  id: number;
+}
+
+interface InfinityScrollData {
+  todos: Todo[];
+  currentPageNumber: number;
+  size: number;
+  hasNext: boolean;
+}
+
+const getInfinityScroll = async (size = '10', page: string) => {
+  const domain = getDomain();
+
+  const params = new URLSearchParams({ size, page }).toString();
+
+  return await fetch(`${domain}/todos?${params}`, {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then(res => {
+      if (!res.ok) {
+        throw new Error(`HTTP error! status: ${res.status}`);
+      }
+      return res.json();
+    })
+    .then(data => data as InfinityScrollData);
+};
+
+export const useGetInfinityScrollAPI = () => {
+  const { data, hasNextPage, fetchNextPage, isFetchingNextPage, isError } =
+    useInfiniteQuery({
+      queryKey: ['InfinityScroll'],
+      queryFn: ({ pageParam }) => getInfinityScroll('10', pageParam.toString()),
+      initialPageParam: 0,
+      getNextPageParam: ({ hasNext, currentPageNumber }) =>
+        hasNext ? currentPageNumber + 1 : undefined,
+    });
+
+  return {
+    todos: data?.pages.map(({ todos }) => todos).flat(),
+    hasNextPage,
+    fetchNextPage,
+    isFetchingNextPage,
+    isError,
+  };
+};
