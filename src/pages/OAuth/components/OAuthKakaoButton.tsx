ba02da7b6d6ff031f020ec81f151e167616241e7
songhaeunsong <gkdms3469@naver.com>
2024-09-04 10:34:56 +0900

    Feat: 카카오로그인 컴포넌트

--- src/pages/OAuth/components/OAuthKakaoButton.tsx
+++ src/pages/OAuth/components/OAuthKakaoButton.tsx
@@ -1,10 +1,10 @@
-import { Link } from 'react-router-dom';
-
 const OAuthKakaoButton = () => {
+  const kakaoUrl = `${import.meta.env.VITE_OAUTH_KAKAO_URL}?response_type=code&client_id=${import.meta.env.VITE_OAUTH_KAKAO_CLIENT_ID}&redirect_uri=${import.meta.env.VITE_OAUTH_KAKAO_REDIRECT_URI}`;
+
   return (
-    <Link to='/' className='w-[100px]'>
+    <a href={kakaoUrl} className='w-[100px]'>
       <img src='kakao_login_large.png' alt='카카오 로그인' />
-    </Link>
+    </a>
   );
 };
 
