2afb782580e41191c051fe4f25b592a078f83135
songhaeunsong <gkdms3469@naver.com>
2024-09-28 19:41:33 +0900

    Refactor: reissue 로직 분리

--- src/apis/client.ts
+++ src/apis/client.ts
@@ -1,13 +1,11 @@
-import ky from 'ky';
+import ky, { KyRequest } from 'ky';
 
 import { toast } from '@/components/toast/use-toast';
 import { getDomain } from '@/utils/domain';
 
 import { getAccessToken } from './authentication';
 
-let accessToken = '';
-
-const ApiClient = ky.create({
+const api = ky.create({
   prefixUrl: `${getDomain()}`,
   headers: {
     'Content-Type': 'application/json',
@@ -15,36 +13,38 @@ const ApiClient = ky.create({
   hooks: {
     beforeRequest: [
       request => {
-        accessToken = getAccessToken();
-        if (accessToken) {
-          request.headers.set('Authorization', `Bearer ${accessToken}`);
+        if (getAccessToken()) {
+          request.headers.set('Authorization', `Bearer ${getAccessToken()}`);
         }
       },
     ],
+  },
+});
+
+const reissue = (request: KyRequest) =>
+  api
+    .post('auth/reissue')
+    .text()
+    .then(token => {
+      request.headers.set('Authorization', `Bearer ${token}`);
+      return ky(request);
+    })
+    .catch(() => {
+      toast({
+        variant: 'destructive',
+        title: 'refresh-token 만료!',
+        description: '재로그인이 필요합니다.',
+      });
+    });
+
+const ApiClient = api.extend({
+  hooks: {
     afterResponse: [
       (_request, _options, response) => response,
       async (request, _options, response) => {
         if (response.ok) return response;
         if (response.status === 401) {
-          return ky
-            .post('auth/reissue', {
-              headers: {
-                Authorization: `Bearer ${accessToken}`,
-                'Content-Type': 'application/json',
-              },
-            })
-            .text()
-            .then(token => {
-              request.headers.set('Authorization', `Bearer ${token}`);
-              return ky(request);
-            })
-            .catch(() => {
-              toast({
-                variant: 'destructive',
-                title: 'refresh-token 만료!',
-                description: '재로그인이 필요합니다.',
-              });
-            });
+          return reissue(request);
         }
       },
     ],
