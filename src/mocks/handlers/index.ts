491c167d62fe6ba72fa01bbcd4ffce1f7ae371cc
HoberMin <102784200+HoberMin@users.noreply.github.com>
2024-10-11 21:12:53 +0900

    feat: FCM

--- src/mocks/handlers/index.ts
+++ src/mocks/handlers/index.ts
@@ -1,5 +1,11 @@
 import { authHandler } from './authHandler';
 import { emailHandler } from './emailHandler';
+import { FCMHandler } from './fcmHandler';
 import { todoHandler } from './todoHandler';
 
-export const handlers = [...todoHandler, ...emailHandler, ...authHandler];
+export const handlers = [
+  ...FCMHandler,
+  ...todoHandler,
+  ...emailHandler,
+  ...authHandler,
+];
--- src/pages/FCM/index.tsx
+++ src/pages/FCM/index.tsx
@@ -1,35 +1,128 @@
-// import firebase from 'firebase/app';
-// import 'firebase/messaging';
+import { useState } from 'react';
 
-// const firebaseConfig = {
-//   apiKey: 'AIzaSyAuJssKqIuXvzU3EEA2tSZi8ss_CexDwgE',
-//   authDomain: 'fanpool-4793d.firebaseapp.com',
-//   projectId: 'fanpool-4793d',
-//   storageBucket: 'fanpool-4793d.appspot.com',
-//   messagingSenderId: '394629578591',
-//   appId: '1:394629578591:web:a2237701e54c5c75c6c0ea',
-//   measurementId: 'G-P4LL243SQS',
-// };
+import firebase from 'firebase/app';
+import 'firebase/messaging';
 
-// const initializeFirebase = () => {
-//   firebase.apps.length
-//     ? firebase.app()
-//     : firebase.initializeApp(firebaseConfig);
-//   const messaging = firebase.messaging();
-//   messaging
-//     .getToken({
-//       vapidKey:
-//         'BClKWaVc65S9Ok6S_D1f-IppgaQTT2phijNKODvFbczK09fQB7sHikeqoLWF43wRBAb9vdYPOklw1L-iyn1HO08',
-//     })
-//     .then(updateFCMToken);
-// };
+import { FirebaseConfig, useGetFCMConfig, usePostFCMToken } from '@/apis/FCM';
+import NotDomainAlertBox from '@/components/NotDomainAlertBox';
+import { Button } from '@/components/button';
+import useDomainStore from '@/store';
 
-// const updateFCMToken = (issuedFCMToken: string) => {
-//   console.log(issuedFCMToken);
-// };
+const initializeFirebase = async (
+  firebaseConfig: FirebaseConfig,
+  vapidKey: string | undefined,
+) => {
+  firebase.apps.length
+    ? firebase.app()
+    : firebase.initializeApp(firebaseConfig);
+  const messaging = firebase.messaging();
+  const token = await messaging.getToken({ vapidKey }).then(res => res);
 
-// const useInitializeFCM = () => {
-//   initializeFirebase();
-// };
+  return token;
+};
 
-// export default useInitializeFCM;
+const FCMPage = () => {
+  const { domain } = useDomainStore();
+  const getConfig = useGetFCMConfig(domain);
+
+  const [firebaseConfig, setFirebaseConfig] = useState<FirebaseConfig>({
+    apiKey: undefined,
+    authDomain: undefined,
+    projectId: undefined,
+    storageBucket: undefined,
+    messagingSenderId: undefined,
+    measurementId: undefined,
+    appId: undefined,
+  });
+  const [vapidKey, setVapidKey] = useState<string | undefined>(undefined);
+  const [deviceToken, setDeviceToken] = useState<string | undefined>(undefined);
+  const postFCMToken = usePostFCMToken(domain);
+
+  if (!domain) {
+    return (
+      <main className='flex h-full w-full flex-col justify-center'>
+        <div className='mx-auto flex w-[600px] flex-col gap-5'>
+          <NotDomainAlertBox />
+        </div>
+      </main>
+    );
+  }
+
+  const handleGetFCMConfig = async () => {
+    const {
+      apiKey,
+      authDomain,
+      projectId,
+      storageBucket,
+      messagingSenderId,
+      appId,
+      measurementId,
+      vapidKey,
+    } = await getConfig();
+
+    setVapidKey(vapidKey);
+
+    setFirebaseConfig(prevState => ({
+      ...prevState,
+      apiKey,
+      authDomain,
+      projectId,
+      storageBucket,
+      messagingSenderId,
+      measurementId,
+      appId,
+    }));
+  };
+
+  const handleGetFCMToken = async () => {
+    if (!firebaseConfig.apiKey) return;
+    const token = await initializeFirebase(firebaseConfig, vapidKey);
+    setDeviceToken(token);
+  };
+
+  const handlePostDeviceToken = async () => {
+    if (!deviceToken) return;
+    await postFCMToken(deviceToken);
+  };
+
+  return (
+    <main className='flex h-full w-full flex-col justify-center'>
+      <div className='mx-auto flex w-[600px] flex-col items-center gap-5 text-wrap rounded-[8px] border border-gray-200 p-[40px] shadow-xl'>
+        <div className='flex w-full flex-col gap-1'>
+          {Object.entries(firebaseConfig).map(([key, value]) => {
+            return (
+              <span className='flex min-w-[300px] gap-2'>
+                <span className='grow font-bold'>{key}</span>
+                <span>{`${value}`}</span>
+              </span>
+            );
+          })}
+        </div>
+        <span className='mt-[50px] w-full break-all text-lg font-bold'>
+          {deviceToken ? deviceToken : '기기토큰이 없습니다'}
+        </span>
+        <div className='flex items-center gap-4'>
+          <Button className='max-w-[200px] p-4' onClick={handleGetFCMConfig}>
+            Get FCM Config
+          </Button>
+          <Button
+            disabled={!firebaseConfig.apiKey}
+            className='max-w-[200px] p-4'
+            onClick={handleGetFCMToken}
+          >
+            Get Device Token
+          </Button>
+          <Button
+            disabled={!deviceToken}
+            className='max-w-[200px] p-4'
+            onClick={handlePostDeviceToken}
+          >
+            POST Device Token
+          </Button>
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default FCMPage;
