2d4b3f99b56c861b116adb5a00c17ec2e5e98b75
HoberMin <102784200+HoberMin@users.noreply.github.com>
2024-10-12 21:02:19 +0900

    style: 각 기능별 헤더 추가

--- src/pages/CRUD/components/TodoContainer.tsx
+++ src/pages/CRUD/components/TodoContainer.tsx
@@ -16,36 +16,44 @@ const TodoContainer = () => {
 
   if (!domain) {
     return (
-      <main className='flex h-full w-full flex-col justify-center'>
-        <div className='mx-auto flex w-[600px] flex-col gap-5'>
-          <NotDomainAlertBox />
-        </div>
-      </main>
+      <>
+        <div className='p-10 pb-0 text-2xl font-bold'>CRUD</div>
+        <main className='flex w-full grow flex-col justify-center'>
+          <div className='mx-auto flex w-[600px] flex-col gap-5'>
+            <NotDomainAlertBox />
+          </div>
+        </main>
+      </>
     );
   }
 
   return (
-    <main className='flex h-full w-full flex-col justify-center'>
-      <div className='mx-auto flex w-[600px] flex-col gap-5'>
-        {!isError && data && (
-          <>
-            <TodoInput />
-            <div className='max-h-[600px] overflow-x-hidden overflow-y-hidden overflow-y-scroll rounded-[8px] border border-gray-200 shadow-xl'>
-              {data.todos.map(({ content, completed, id }) => (
-                <TodoItem
-                  checked={completed}
-                  todo={content}
-                  todoId={id}
-                  key={`${content}-${id}`}
-                />
-              ))}
-            </div>
-            <span className='mt-[40px]'>Made By HoberMin / songhaeunsong</span>
-          </>
-        )}
-        {isError && <AlertBox />}
-      </div>
-    </main>
+    <>
+      <div className='p-10 pb-0 text-2xl font-bold'>CRUD</div>
+      <main className='flex w-full grow flex-col justify-center'>
+        <div className='mx-auto flex w-[600px] flex-col gap-5'>
+          {!isError && data && (
+            <>
+              <TodoInput />
+              <div className='max-h-[600px] overflow-x-hidden overflow-y-hidden overflow-y-scroll rounded-[8px] border border-gray-200 shadow-xl'>
+                {data.todos.map(({ content, completed, id }) => (
+                  <TodoItem
+                    checked={completed}
+                    todo={content}
+                    todoId={id}
+                    key={`${content}-${id}`}
+                  />
+                ))}
+              </div>
+              <span className='mt-[40px]'>
+                Made By HoberMin / songhaeunsong
+              </span>
+            </>
+          )}
+          {isError && <AlertBox />}
+        </div>
+      </main>
+    </>
   );
 };
 
--- src/pages/Email/index.tsx
+++ src/pages/Email/index.tsx
@@ -98,86 +98,98 @@ const Email = () => {
 
   if (!domain) {
     return (
-      <main className='flex h-full w-full flex-col justify-center'>
-        <div className='mx-auto flex w-[600px] flex-col gap-5'>
-          <NotDomainAlertBox />
-        </div>
-      </main>
+      <>
+        <div className='p-10 pb-0 text-2xl font-bold'>Email</div>
+
+        <main className='flex w-full grow flex-col justify-center'>
+          <div className='mx-auto flex w-[600px] flex-col gap-5'>
+            <NotDomainAlertBox />
+          </div>
+        </main>
+      </>
     );
   }
 
   return (
-    <main className='flex h-full w-full flex-col justify-center'>
-      <div className='mx-auto flex w-[600px] flex-col gap-5'>
-        <div className='flex max-h-[600px] flex-col gap-[20px] overflow-x-hidden overflow-y-hidden overflow-y-scroll rounded-[8px] border border-gray-200 p-[60px] shadow-xl'>
-          <Form {...emailForm}>
-            <form onSubmit={emailForm.handleSubmit(onEmailSubmit)}>
-              <FormField
-                control={emailForm.control}
-                name='email'
-                render={({ field }) => (
-                  <FormItem className='flex flex-col gap-0.5'>
-                    <FormLabel className='pl-1'>이메일</FormLabel>
-                    <div className='flex items-center gap-[10px]'>
-                      <FormControl>
-                        <Input placeholder='이메일을 입력하세요' {...field} />
-                      </FormControl>
-                      <Button type='submit' className='px-4 py-2 text-sm'>
-                        {isOk ? '재인증' : '인증'}
-                      </Button>
-                    </div>
-                    <FormMessage className='pl-1' />
-                  </FormItem>
+    <>
+      <div className='p-10 pb-0 text-2xl font-bold'>Email</div>
+      <main className='flex w-full grow flex-col justify-center'>
+        <div className='mx-auto flex w-[600px] flex-col gap-5'>
+          <div className='flex max-h-[600px] flex-col gap-[20px] overflow-x-hidden overflow-y-hidden overflow-y-scroll rounded-[8px] border border-gray-200 p-[60px] shadow-xl'>
+            <Form {...emailForm}>
+              <form onSubmit={emailForm.handleSubmit(onEmailSubmit)}>
+                <FormField
+                  control={emailForm.control}
+                  name='email'
+                  render={({ field }) => (
+                    <FormItem className='flex flex-col gap-0.5'>
+                      <FormLabel className='pl-1'>이메일</FormLabel>
+                      <div className='flex items-center gap-[10px]'>
+                        <FormControl>
+                          <Input placeholder='이메일을 입력하세요' {...field} />
+                        </FormControl>
+                        <Button type='submit' className='px-4 py-2 text-sm'>
+                          {isOk ? '재인증' : '인증'}
+                        </Button>
+                      </div>
+                      <FormMessage className='pl-1' />
+                    </FormItem>
+                  )}
+                />
+              </form>
+            </Form>
+            {isOk && !isSuccess && (
+              <span className='text-sm text-blue-500'>
+                남은 시간: {formatTime(timer)}
+              </span>
+            )}
+            <Form {...authenticationForm}>
+              <form
+                onSubmit={authenticationForm.handleSubmit(
+                  onAuthenticationSubmit,
                 )}
-              />
-            </form>
-          </Form>
-          {isOk && !isSuccess && (
-            <span className='text-sm text-blue-500'>
-              남은 시간: {formatTime(timer)}
-            </span>
-          )}
-          <Form {...authenticationForm}>
-            <form
-              onSubmit={authenticationForm.handleSubmit(onAuthenticationSubmit)}
-            >
-              <FormField
-                control={authenticationForm.control}
-                name='authentication'
-                render={({ field }) => (
-                  <FormItem className='flex flex-col gap-0.5'>
-                    <FormLabel className='pl-1'>인증번호</FormLabel>
-                    <div className='flex items-center gap-[10px]'>
-                      <FormControl>
-                        <Input placeholder='인증번호를 입력하세요' {...field} />
-                      </FormControl>
-                      <Button
-                        type='submit'
-                        className='px-4 py-2 text-sm'
-                        disabled={isButtonDisabled || isSuccess} // 버튼 비활성화 로직
-                      >
-                        확인
-                      </Button>
-                    </div>
-                    <FormMessage className='pl-1' />
-                    {isSuccess && (
-                      <FormLabel className='pl-1 text-green-500'>
-                        인증에 성공했습니다!
-                      </FormLabel>
-                    )}
-                    {isAuthenticationError && (
-                      <FormLabel className='pl-1 text-destructive'>
-                        잘못된 인증번호입니다.
-                      </FormLabel>
-                    )}
-                  </FormItem>
-                )}
-              />
-            </form>
-          </Form>
+              >
+                <FormField
+                  control={authenticationForm.control}
+                  name='authentication'
+                  render={({ field }) => (
+                    <FormItem className='flex flex-col gap-0.5'>
+                      <FormLabel className='pl-1'>인증번호</FormLabel>
+                      <div className='flex items-center gap-[10px]'>
+                        <FormControl>
+                          <Input
+                            placeholder='인증번호를 입력하세요'
+                            {...field}
+                          />
+                        </FormControl>
+                        <Button
+                          type='submit'
+                          className='px-4 py-2 text-sm'
+                          disabled={isButtonDisabled || isSuccess} // 버튼 비활성화 로직
+                        >
+                          확인
+                        </Button>
+                      </div>
+                      <FormMessage className='pl-1' />
+                      {isSuccess && (
+                        <FormLabel className='pl-1 text-green-500'>
+                          인증에 성공했습니다!
+                        </FormLabel>
+                      )}
+                      {isAuthenticationError && (
+                        <FormLabel className='pl-1 text-destructive'>
+                          잘못된 인증번호입니다.
+                        </FormLabel>
+                      )}
+                    </FormItem>
+                  )}
+                />
+              </form>
+            </Form>
+          </div>
         </div>
-      </div>
-    </main>
+      </main>
+    </>
   );
 };
 export default Email;
