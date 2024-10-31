4e9446e7a59c875e2956ec5a2060bd7ea528af35
songhaeunsong <gkdms3469@naver.com>
2024-10-31 10:14:48 +0900

    feat: 탭에 따라 인가코드 전송 분리

--- src/pages/Redirect/index.tsx
+++ src/pages/Redirect/index.tsx
@@ -2,19 +2,29 @@ import { useEffect } from 'react';
 
 import { useSearchParams } from 'react-router-dom';
 
-import { usePostCodeApi } from '@/apis/authentication';
-import useDomainStore from '@/store';
+import {
+  usePostCodeApi,
+  usePostCodeWithAuthorizationApi,
+  usePostCodeWithCookieApi,
+} from '@/apis/authentication';
+import useDomainStore, { useTokenTypeStore } from '@/store';
 
 const RedirectPage = () => {
   const [searchParams] = useSearchParams();
+  const { tokenType } = useTokenTypeStore();
 
   const code = searchParams.get('code');
   const { domain } = useDomainStore();
+
   const postCode = usePostCodeApi(domain);
+  const postCodeWithAuthorization = usePostCodeWithAuthorizationApi(domain);
+  const postCodeWithCookie = usePostCodeWithCookieApi(domain);
 
   useEffect(() => {
-    if (code) {
-      postCode(code);
+    if (code && tokenType) {
+      if (tokenType === 1) postCode(code);
+      else if (tokenType === 2) postCodeWithAuthorization(code);
+      else postCodeWithCookie(code);
     }
   }, [code]);
 
